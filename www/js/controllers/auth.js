4dad7b8f3041a34669c4e61a4cefbce2218dbcf5
SomeDevelop <v.bohdanovskyi@gmail.com>
2018-04-29 09:48:30 +0300

    parse personal information of the facebook user

--- www/js/controllers/auth.js
+++ www/js/controllers/auth.js
@@ -18,104 +18,23 @@ firebase.initializeApp(config);
 
 
 var provider = new firebase.auth.FacebookAuthProvider();
-provider.addScope('user_birthday');
+provider.addScope('user_location');
 
-firebase.auth().signInWithPopup(provider).then(function(authData) {
-	console.log(authData);
+firebase.auth().signInWithPopup(provider).then(function(result) {
+	console.log("Name: ", result.additionalUserInfo.profile.name);
+  	console.log("City: ", result.additionalUserInfo.profile.location.name);
+	console.log("Age: ", result.additionalUserInfo.profile.age_range.min);
+	console.log("Genger: ", result.additionalUserInfo.profile.gender);
+	console.log("email: ", result.additionalUserInfo.profile.email);
+
+
+
+  console.log(result);
 }).catch(function(error) {
-	console.log(error);
-});
-provider.setCustomParameters({
-  'display': 'popup'
+  console.error("Authentication failed:", error);
 });
 
-// var provider = new firebase.auth.FacebookAuthProvider();
-// provider.addScope('user_birthday');
 
-// provider.setCustomParameters({
-//   'display': 'popup'
-// });
-
-// firebase.auth().signInWithPopup(provider).then(function(authData) {
-// 	 			console.log(authData);
-// 	 		}).catch(function(error){
-// 	 			console.log(error);
-// 	 		});
-
-// firebase.auth().signInWithPopup(provider).then(function(result) {
-//   // This gives you a Facebook Access Token. You can use it to access the Facebook API.
-//   var token = result.credential.accessToken;
-//   // The signed-in user info.
-//   var user = result.user;
-//   // ...
-// }).catch(function(error) {
-//   // Handle Errors here.
-//   var errorCode = error.code;
-//   var errorMessage = error.message;
-//   // The email of the user's account used.
-//   var email = error.email;
-//   // The firebase.auth.AuthCredential type that was used.
-//   var credential = error.credential;
-//   // ...
-// });
-
-// firebase.auth().signInWithRedirect(provider);
-
-// firebase.auth().getRedirectResult().then(function(result) {
-//   if (result.credential) {
-//     // This gives you a Facebook Access Token. You can use it to access the Facebook API.
-//     var token = result.credential.accessToken;
-//     // ...
-//   }
-//   // The signed-in user info.
-//   var user = result.user;
-// }).catch(function(error) {
-//   // Handle Errors here.
-//   var errorCode = error.code;
-//   var errorMessage = error.message;
-//   // The email of the user's account used.
-//   var email = error.email;
-//   // The firebase.auth.AuthCredential type that was used.
-//   var credential = error.credential;
-//   // ...
-// });
-
-
-// console.log(authData);
-
-	
-
-	// var Auth = {
-	// 	login: function(){
-	// 		return firebase.signInWithPopup(provider).then(function(authData) {
-	// 			console.log(authData);
-	// 		}).catch(function(error){
-	// 			console.log(error);
-	// 		});
-	// 	},
-
-	// 	logout: function(){
-	// 		return auth.$unauth();
-	// 	}
-	// };
-
-	
-
-	
-
-	// auth.$onAuth(function(authData){
-	// 	if(authData){
-	// 		console.log('Logged in!');
-	// 	} else{
-	// 		$state.go('login');
-	// 		console.log('You need to login.');
-	// 	}
-	// })
-
-	// return Auth;
-	
-		// var user = Auth.login();
-		// console.log(user.facebook);
 };
 
 	auth.logout = function(){
